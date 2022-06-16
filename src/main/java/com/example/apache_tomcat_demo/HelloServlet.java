192c006d0546b4b0731c4a8d3d72c26b0781aa66
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-16 19:53:26 +0530

    call the resp.setContentType() method

--- src/main/java/com/example/apache_tomcat_demo/HelloServlet.java
+++ src/main/java/com/example/apache_tomcat_demo/HelloServlet.java
@@ -18,6 +18,9 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("text/html");
+
         PrintWriter out = resp.getWriter();
         out.println("<h1> Hello, World! </h1>");
         out.close();
