846aa712f3a000931186711d5fe391930d1cf155
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-16 20:16:30 +0530

    initialize a field of type PrintWriter with the response.getWriter() method call

--- src/main/java/com/example/apache_tomcat_demo/TableServlet.java
+++ src/main/java/com/example/apache_tomcat_demo/TableServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "TableServlet", value = "/table")
 public class TableServlet extends HttpServlet {
@@ -13,5 +14,6 @@ public class TableServlet extends HttpServlet {
         int num = Integer.parseInt(input);
 
         response.setContentType("text/html");
+        PrintWriter output = response.getWriter();
     }
 }
