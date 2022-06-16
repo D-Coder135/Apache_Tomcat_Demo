45fc294f571439079302423afd3441199923c418
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-16 20:19:22 +0530

    call the println() method

--- src/main/java/com/example/apache_tomcat_demo/TableServlet.java
+++ src/main/java/com/example/apache_tomcat_demo/TableServlet.java
@@ -16,6 +16,7 @@ public class TableServlet extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter output = response.getWriter();
 
+        output.println("<h3> Table for " + num + ": </h3>");
         for(int  i  = 1; i <= 10; i++) {
             output.printf("%d X %d = %d <br>", num, i, num * i);
         }
