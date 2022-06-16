b3588627aa5d105ad6038dd32fe440b1b7d36c95
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-16 19:52:55 +0530

    call the close() method

--- src/main/java/com/example/apache_tomcat_demo/HelloServlet.java
+++ src/main/java/com/example/apache_tomcat_demo/HelloServlet.java
@@ -20,5 +20,6 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
         out.println("<h1> Hello, World! </h1>");
+        out.close();
     }
 }
