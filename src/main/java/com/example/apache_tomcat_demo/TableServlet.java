2a1237fa222fd9e693ba0dd894b572ac5d20ff40
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-16 22:28:55 +0530

    add return keyword

--- src/main/java/com/example/apache_tomcat_demo/TableServlet.java
+++ src/main/java/com/example/apache_tomcat_demo/TableServlet.java
@@ -14,6 +14,7 @@ public class TableServlet extends HttpServlet {
 
         if (input == null) {
             response.sendRedirect("table.html");
+            return;
         }
 
         int num = Integer.parseInt(input);
