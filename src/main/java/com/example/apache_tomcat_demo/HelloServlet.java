d152fc66f75284bc08004ae20eaaf5a5aa5905a8
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-16 20:00:48 +0530

    add the annotation @WebServlet inside the HelloServlet.java

--- src/main/java/com/example/apache_tomcat_demo/HelloServlet.java
+++ src/main/java/com/example/apache_tomcat_demo/HelloServlet.java
@@ -8,12 +8,14 @@
 package com.example.apache_tomcat_demo;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet("/hello-world")
 public class HelloServlet extends HttpServlet {
 
     @Override
