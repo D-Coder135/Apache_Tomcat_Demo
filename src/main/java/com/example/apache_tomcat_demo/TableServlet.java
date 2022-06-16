2aea69ca1336718fd005fc3957fc6c051f810f3c
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-16 20:14:50 +0530

    initialize a field of type int with the Integer.parseInt() method call

--- src/main/java/com/example/apache_tomcat_demo/TableServlet.java
+++ src/main/java/com/example/apache_tomcat_demo/TableServlet.java
@@ -10,5 +10,6 @@ public class TableServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String input = request.getParameter("num");
+        int num = Integer.parseInt(input);
     }
 }
