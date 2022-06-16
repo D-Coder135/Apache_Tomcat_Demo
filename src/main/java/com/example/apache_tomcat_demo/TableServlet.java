fc359b5b1cebdae03b9dbcb619ffd431538b1061
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-16 20:11:40 +0530

    update the value parameter inside the @WebServer annotation

--- src/main/java/com/example/apache_tomcat_demo/TableServlet.java
+++ src/main/java/com/example/apache_tomcat_demo/TableServlet.java
@@ -5,7 +5,7 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "TableServlet", value = "/TableServlet")
+@WebServlet(name = "TableServlet", value = "/table")
 public class TableServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
