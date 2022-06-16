6d8d212e157700c3763c35e97d147cea0386c150
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-16 19:52:44 +0530

    call the println() method

--- src/main/java/com/example/apache_tomcat_demo/HelloServlet.java
+++ src/main/java/com/example/apache_tomcat_demo/HelloServlet.java
@@ -19,5 +19,6 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
+        out.println("<h1> Hello, World! </h1>");
     }
 }
