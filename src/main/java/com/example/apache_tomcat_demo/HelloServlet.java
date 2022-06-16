97744108dfef846927027977625d0cdead3baf92
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-16 19:49:12 +0530

    add a new class HelloServlet.java inside the package com.example.apache_tomcat_demo

---
+++ src/main/java/com/example/apache_tomcat_demo/HelloServlet.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 16-06-2022/06/2022
+ *   Time: 07:48 PM
+ *   File: HelloServelet
+ */
+
+package com.example.apache_tomcat_demo;
+
+public class HelloServlet {
+}
