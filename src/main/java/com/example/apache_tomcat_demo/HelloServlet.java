e231cbf854d20861d962aad7826401bbc8fde972
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-16 19:51:07 +0530

    remove some pre-written code inside the doGet() method body

--- src/main/java/com/example/apache_tomcat_demo/HelloServlet.java
+++ src/main/java/com/example/apache_tomcat_demo/HelloServlet.java
@@ -17,6 +17,6 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+
     }
 }
