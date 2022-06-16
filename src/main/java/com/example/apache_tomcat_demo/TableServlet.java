4c18c6231f319797f65ea31e3b672f11904f3979
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-16 20:11:54 +0530

    remove the doPost() method

--- src/main/java/com/example/apache_tomcat_demo/TableServlet.java
+++ src/main/java/com/example/apache_tomcat_demo/TableServlet.java
@@ -11,9 +11,4 @@ public class TableServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 }
