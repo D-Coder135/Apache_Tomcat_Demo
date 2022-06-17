fa1c96ccea6cf643c5b30e7db2f88c4835b8c539
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-17 23:13:34 +0530

    initialize a field of type String with the getParameter() method call

--- src/main/java/com/example/apache_tomcat_demo/AddNameServlet.java
+++ src/main/java/com/example/apache_tomcat_demo/AddNameServlet.java
@@ -10,5 +10,6 @@ public class AddNameServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        String friendName = request.getParameter("friend_name");
     }
 }
