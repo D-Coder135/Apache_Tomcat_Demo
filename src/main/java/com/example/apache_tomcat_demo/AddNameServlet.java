1caf37db9f2644d89118dc97a502547e8793a0c3
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-17 23:12:16 +0530

    add a new servlet AddNameServlet.java

---
+++ src/main/java/com/example/apache_tomcat_demo/AddNameServlet.java
@@ -0,0 +1,19 @@
+package com.example.apache_tomcat_demo;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "add-name", value = "/add-name")
+public class AddNameServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
