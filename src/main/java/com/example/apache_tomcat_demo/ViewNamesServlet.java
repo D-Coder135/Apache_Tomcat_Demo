9fdc5e9e5c9cfa3a4917fd49de5fae71b5449e3a
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-17 23:36:58 +0530

    initialize a field of type HttpSession with the getSession() method call

--- src/main/java/com/example/apache_tomcat_demo/ViewNamesServlet.java
+++ src/main/java/com/example/apache_tomcat_demo/ViewNamesServlet.java
@@ -10,5 +10,6 @@ public class ViewNamesServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        HttpSession session = request.getSession();
     }
 }
