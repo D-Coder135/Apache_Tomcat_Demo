3b3ab3bb79adfec33aa56f87576da47464dfeca0
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-17 23:12:38 +0530

    remove the doPost() method

--- src/main/java/com/example/apache_tomcat_demo/AddNameServlet.java
+++ src/main/java/com/example/apache_tomcat_demo/AddNameServlet.java
@@ -11,9 +11,4 @@ public class AddNameServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    }
 }
