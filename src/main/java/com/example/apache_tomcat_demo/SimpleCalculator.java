e9933d82250a64793e2d02b6f28e593e9410b086
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-17 14:13:01 +0530

    remove the doPost() method

--- src/main/java/com/example/apache_tomcat_demo/SimpleCalculator.java
+++ src/main/java/com/example/apache_tomcat_demo/SimpleCalculator.java
@@ -12,8 +12,5 @@ public class SimpleCalculator extends HttpServlet {
 
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    }
 }
