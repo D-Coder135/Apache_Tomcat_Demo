3456503cc7c1f9239ec8bee8e44c0047f8e8657b
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-17 14:40:16 +0530

    add a new class PrintResultServlet.java inside the package com.example.apache_tomcat_demo

---
+++ src/main/java/com/example/apache_tomcat_demo/PrintResultServlet.java
@@ -0,0 +1,19 @@
+package com.example.apache_tomcat_demo;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "print-result", value = "/print-result")
+public class PrintResultServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
