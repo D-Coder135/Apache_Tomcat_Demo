867c8284524853a8c1789a065ee5fd8e36102b66
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-19 23:16:53 +0530

    add a new class TableTag.java inside the package com.example.apache_tomcat_demo

---
+++ src/main/java/com/example/apache_tomcat_demo/TableTag.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 19-06-2022/06/2022
+ *   Time: 11:16 PM
+ *   File: TableTag
+ */
+
+package com.example.apache_tomcat_demo;
+
+public class TableTag {
+}
