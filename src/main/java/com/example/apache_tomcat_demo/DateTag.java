a243cd39f489d8722406486aea5bffcc086f61a3
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-19 13:11:20 +0530

    override the doStartTag() method from the TagSupport class

--- src/main/java/com/example/apache_tomcat_demo/DateTag.java
+++ src/main/java/com/example/apache_tomcat_demo/DateTag.java
@@ -6,7 +6,13 @@
  */
 
 package com.example.apache_tomcat_demo;
+import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.TagSupport;
 
 public class DateTag extends TagSupport {
+
+    @Override
+    public int doStartTag() throws JspException {
+        return super.doStartTag();
+    }
 }
