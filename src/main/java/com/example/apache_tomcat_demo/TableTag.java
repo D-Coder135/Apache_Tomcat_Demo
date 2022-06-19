6e9156b89518fa37e341f4235d425d877cc59625
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-19 23:18:23 +0530

    override the doAfterBody() method from the BodyTagSupport class

--- src/main/java/com/example/apache_tomcat_demo/TableTag.java
+++ src/main/java/com/example/apache_tomcat_demo/TableTag.java
@@ -7,7 +7,13 @@
 
 package com.example.apache_tomcat_demo;
 
+import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.BodyTagSupport;
 
 public class TableTag extends BodyTagSupport {
+
+    @Override
+    public int doAfterBody() throws JspException {
+        return super.doAfterBody();
+    }
 }
