f27065c8056d436eabe18d2eb0cac80fc4f45abf
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-19 13:12:48 +0530

    initialize a field of type JspWriter with the getOut() method call

--- src/main/java/com/example/apache_tomcat_demo/DateTag.java
+++ src/main/java/com/example/apache_tomcat_demo/DateTag.java
@@ -7,12 +7,16 @@
 
 package com.example.apache_tomcat_demo;
 import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.TagSupport;
 
 public class DateTag extends TagSupport {
 
     @Override
     public int doStartTag() throws JspException {
+
+        JspWriter out = pageContext.getOut();
+
         return super.doStartTag();
     }
 }
