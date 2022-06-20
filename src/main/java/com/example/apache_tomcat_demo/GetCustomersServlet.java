eab356a90ec537f21410ab65c75be5cf00860c4b
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-20 23:57:59 +0530

    initialize an object of type List<Customer> with the getAllCustomers() method call

--- src/main/java/com/example/apache_tomcat_demo/GetCustomersServlet.java
+++ src/main/java/com/example/apache_tomcat_demo/GetCustomersServlet.java
@@ -2,11 +2,13 @@ package com.example.apache_tomcat_demo;
 
 import com.example.apache_tomcat_demo.dao.CustomerDao;
 import com.example.apache_tomcat_demo.dao.DaoFactory;
+import com.example.apache_tomcat_demo.entity.Customer;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "getCustomers", value = "/getCustomers")
 public class GetCustomersServlet extends HttpServlet {
@@ -14,6 +16,7 @@ public class GetCustomersServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         CustomerDao dao = DaoFactory.getCustomerDao();
+        List<Customer> list = dao.getAllCustomers();
 
     }
 }
