732edb0de631c40a341247a61b9faadd8435d220
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-21 00:20:57 +0530

    update the value attribute inside the @WebServlet annotation

--- src/main/java/com/example/apache_tomcat_demo/GetCustomersServlet.java
+++ src/main/java/com/example/apache_tomcat_demo/GetCustomersServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(name = "getCustomers", value = "/getCustomers")
+@WebServlet(name = "getCustomers", value = "/view-customers")
 public class GetCustomersServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
