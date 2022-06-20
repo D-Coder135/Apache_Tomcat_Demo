d7872de15a2c86b11ba76c052ec3a48042b30994
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-20 23:37:14 +0530

    add a new class DaoFactory.java inside the package com.example.apache_tomcat_demo.dao

---
+++ src/main/java/com/example/apache_tomcat_demo/dao/DaoFactory.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 20-06-2022/06/2022
+ *   Time: 11:36 PM
+ *   File: DaoFactory
+ */
+
+package com.example.apache_tomcat_demo.dao;
+
+public class DaoFactory {
+}
