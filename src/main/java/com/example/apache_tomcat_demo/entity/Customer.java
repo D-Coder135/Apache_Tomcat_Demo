73063fe1277f5a4f37d86b52e4f81b9b6733f874
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-20 23:29:36 +0530

    add a new class Customer.java inside the package com.example.apache_tomcat_demo.entity

---
+++ src/main/java/com/example/apache_tomcat_demo/entity/Customer.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 20-06-2022/06/2022
+ *   Time: 11:29 PM
+ *   File: Customer
+ */
+
+package com.example.apache_tomcat_demo.entity;
+
+public class Customer {
+}
