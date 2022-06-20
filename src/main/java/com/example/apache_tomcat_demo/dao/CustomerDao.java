43f73dbd093a9a83a44b01e5238f144396def137
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-20 23:34:11 +0530

    add an abstract method getAllCustomers() of type List<Customer>

--- src/main/java/com/example/apache_tomcat_demo/dao/CustomerDao.java
+++ src/main/java/com/example/apache_tomcat_demo/dao/CustomerDao.java
@@ -1,4 +1,11 @@
 package com.example.apache_tomcat_demo.dao;
 
+import com.example.apache_tomcat_demo.entity.Customer;
+
+import java.util.List;
+
 public interface CustomerDao {
+
+    public List<Customer> getAllCustomers();
+
 }
