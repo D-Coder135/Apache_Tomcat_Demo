07dc612a60ca6f3e8c1c25188ae2ef87f08754d6
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-06-20 12:07:30 +0530

    add a new class Address.java inside the package com.example.apache_tomcat_demo

---
+++ src/main/java/com/example/apache_tomcat_demo/Address.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Devansh Shukla (D-Coder135)
+ *   Date: 20-06-2022/06/2022
+ *   Time: 12:07 PM
+ *   File: Address
+ */
+
+package com.example.apache_tomcat_demo;
+
+public class Address {
+}
